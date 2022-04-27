9981cda2c21ca9f6d83f42183c060df3e9594203
EpicRaisin <65032841+LameGrape@users.noreply.github.com>
2022-04-27 13:56:04 -0500

    v1.0.0 Initial release
    Initial public release of Gooeey.

---
+++ Plugin.cs
@@ -0,0 +1,41 @@
+﻿using BepInEx;
+using UnityEngine;
+using TMPro;
+using System;
+using System.Collections.Generic;
+
+namespace Gooeey
+{
+    [BepInPlugin("raisin.plugins.gooeey", PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public List<TextMeshProUGUI> textObjects = new List<TextMeshProUGUI>();
+        private void Awake()
+        {
+            Logger.LogInfo($"raisin.plugins.gooeey v{PluginInfo.PLUGIN_VERSION} - Gooeey successfully loaded.");
+        }
+
+        public TextMeshProUGUI CreateText(string name, string text){ // Create a new Text object
+            GameObject textObject = new GameObject(name);
+            textObject.AddComponent<Canvas>().renderMode = RenderMode.ScreenSpaceOverlay;
+            TextMeshProUGUI tmp = textObject.AddComponent<TextMeshProUGUI>();
+
+            tmp.text = text;
+            tmp.fontSize = 30;
+            tmp.color = Color.white;
+            tmp.fontStyle = FontStyles.Bold;
+            tmp.alignment = TextAlignmentOptions.Center;
+
+            textObjects.Add(tmp);
+            return tmp;
+        }
+        public TextMeshProUGUI FindText(string name){
+            foreach(TextMeshProUGUI tmp in textObjects){
+                if(tmp.gameObject.name == name){
+                    return tmp;
+                }
+            }
+            return null;
+        }
+    }
+}
